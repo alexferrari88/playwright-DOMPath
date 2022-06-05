255d53e08e1c019891ad418fa0a52a2d1d833623
Alex Ferrari <a.ferrari88@gmail.com>
2022-06-05 04:39:53 +0200

    Add linting

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+﻿module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint", "import"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/recommended",
+    "plugin:import/typescript",
+    "airbnb-base",
+    "prettier",
+  ],
+  rules: {
+    // turn on errors for missing imports
+    "import/no-unresolved": "error",
+  },
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
+  },
+};
